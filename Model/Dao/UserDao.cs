50676dea17a387bb6e6de32229223523273adb3f
hung.t.ngo <hung.t.ngo@HUNG-T-NGO04>
2020-02-13 09:12:00 +0700

    add Areas Admin

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    class UserDao
+    {
+        OnlienShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlienShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public bool Login(string userName,string passWord)
+        {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == passWord);
+            if(result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
