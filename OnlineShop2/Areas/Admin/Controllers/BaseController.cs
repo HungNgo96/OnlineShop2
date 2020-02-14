da29885801fbfc066949bff5f6cf651f10e1e3ee
hung.t.ngo <hung.t.ngo@HUNG-T-NGO04>
2020-02-14 17:52:10 +0700

    add BaseController

---
+++ OnlineShop2/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using OnlineShop2.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineShop2.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (UserLogin)Session[Common.CommonConstains.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login", action = "Index", Area = "Admin" }));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
--- OnlineShop2/Areas/Admin/Controllers/HomeController.cs
+++ OnlineShop2/Areas/Admin/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace OnlineShop2.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         // GET: Admin/Home
         public ActionResult Index()
